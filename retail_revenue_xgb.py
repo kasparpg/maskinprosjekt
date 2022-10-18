c4f286d1199e15b847d19e3c40d18f205ecc14a8
Jobb <Jobb@Kaspars-MacBook-Pro.local>
2022-10-18 11:28:44 +0200

    embedded store_name attempt

--- retail_revenue_xgb.py
+++ retail_revenue_xgb.py
@@ -5,6 +5,9 @@ import os.path
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import gensim
+from gensim.models import Word2Vec
+from gensim.models.doc2vec import Doc2Vec, TaggedDocument
 from sklearn.cluster import KMeans
 from xgboost import plot_importance
 import warnings
@@ -31,14 +34,14 @@ print(train.info())
 
 print(busstops.info())
 
-def plot_data(df, store_id):
-    df_ = df[['store_id', 'lat', 'lon', 'all_households', 'singles']]
+def plot_data(df):
+    df_ = df[['grunnkrets_id', 'age_children', 'age_youth']]
     fig, axs = plt.subplots(2, 1, sharex=True)
-    sns.lineplot(df_['store_id'], df_['all_households'], ax=axs[0], color='r')
-    # sns.lineplot(df_['store_id'], df_['singles'], ax=axs[1], color='b')
+    sns.lineplot(df_['grunnkrets_id'], df_['age_children'], ax=axs[0], color='r')
+    sns.lineplot(df_['grunnkrets_id'], df_['age_youth'], ax=axs[1], color='b')
     plt.xticks(rotation=90)
-    # plt.scatter(df['lat'], df['lon'])
-
+    # plt.scatter(df['all_households'], df['revenue'])
+    plt.show()
     """
     for i in np.unique(df['clusters']):
         plt.scatter(df[['clusters'] == i, 0], df[['clusters'] == i, 1], label=i)
@@ -70,19 +73,51 @@ def generate_features(df, spatial_features, age_features, income_features,
 
     # one hot encoding
 
-
+    """
     y = pd.get_dummies(df.chain_name, prefix='Chain')
     df.append(y)
+    """
 
     # # # # # # # # # # # # #
     # # FEATURE SELECTION # #
 
     df['store_name'] = df['store_name'].astype('category')
+    """
+    if os.path.exists('modeling/word2vec_model'):
+        word2vec_model = gensim.models.Word2Vec.load('modeling/word2vec_model')
+    else:
+        word2vec_model = Word2Vec(sentences=df['store_name'], min_count=1, vector_size=200, workers=4)
+        word2vec_model.save('modeling/word2vec_model')
+    """
+    """
+    card_docs = [TaggedDocument(doc.split(' '), [i])
+                 for i, doc in enumerate(df.store_name)]
+    
+    doc2vec_model = Doc2Vec(min_count=1, vector_size=5, workers=4, epochs=40)
+    doc2vec_model.build_vocab(card_docs)
+    doc2vec_model.train(card_docs, total_examples=doc2vec_model.corpus_count, epochs=doc2vec_model.epochs)
+
+    card2vec = [doc2vec_model.infer_vector((df['store_name'][i].split(' ')))
+                for i in range(0, len(df['store_name']))]
+    dtv = np.array(card2vec)
+    df['store_name_embedded1'] = dtv[:, 0]
+    df['store_name_embedded2'] = dtv[:, 1]
+    df['store_name_embedded3'] = dtv[:, 2]
+    df['store_name_embedded4'] = dtv[:, 3]
+    df['store_name_embedded5'] = dtv[:, 4]
+    df = df.drop(columns=['store_name'])
+    """
+
+    """
+    y = pd.get_dummies(df.store_name, prefix='store')
+    df.append(y)
+    """
+
     df['store_id'] = df['store_id'].astype('category')
     df['address'] = df['address'].astype('category')
     df['mall_name'] = df['mall_name'].astype('category')
     df['chain_name'] = df['chain_name'].astype('category')
-    df.drop(columns=["chain_name"])  # drop as we have one hot encoded it
+    # df = df.drop(columns=["chain_name"])  # drop as we have one hot encoded it
     df['plaace_hierarchy_id'] = df['plaace_hierarchy_id'].astype('category')
 
     spatial_features.drop_duplicates(subset=['grunnkrets_id'])  # remove duplicates from spatial data
@@ -95,16 +130,36 @@ def generate_features(df, spatial_features, age_features, income_features,
 
     age_features.drop_duplicates(subset=['grunnkrets_id'])  # remove duplicates from age data
     df = pd.merge(df, age_features.drop_duplicates(subset=['grunnkrets_id']), how='left')  # merge with the age data.
-    age_children = ['age_0,age_1,age_2,age_3,age_4,age_5,age_6,age_7,age_8,age_9,'
-                    'age_10,age_11,age_12,age_13,age_14,age_15,age_16']
-    df['age0-16'] = df[age_children].sum(axis=1)
-    df.drop(age_children)
+    age_children = ['age_0', 'age_1', 'age_2', 'age_3', 'age_4', 'age_5', 'age_6', 'age_7', 'age_8', 'age_9',
+                    'age_10', 'age_11', 'age_12', 'age_13', 'age_14']
+    age_youth = ['age_15', 'age_16', 'age_17', 'age_18', 'age_19', 'age_20', 'age_21', 'age_22', 'age_23', 'age_24']
+    age_adults = ['age_25', 'age_26', 'age_27', 'age_28', 'age_29', 'age_30', 'age_31', 'age_32', 'age_33', 'age_34',
+                  'age_35', 'age_36', 'age_37', 'age_38', 'age_39', 'age_40', 'age_41', 'age_42', 'age_43', 'age_44',
+                  'age_45', 'age_46', 'age_47', 'age_48', 'age_49', 'age_50', 'age_51', 'age_52', 'age_53', 'age_54',
+                  'age_55', 'age_56', 'age_57', 'age_58', 'age_59', 'age_60', 'age_61', 'age_62', 'age_63', 'age_64']
+    age_senior = ['age_65', 'age_66', 'age_67', 'age_68', 'age_69', 'age_70', 'age_71', 'age_72',
+                  'age_73', 'age_74', 'age_75', 'age_76', 'age_77', 'age_78', 'age_79', 'age_80', 'age_81',
+                  'age_82', 'age_83', 'age_84', 'age_85', 'age_86', 'age_87', 'age_88', 'age_89', 'age_90']
+    df['age_children'] = df[age_children].sum(axis=1)
+    df['age_youth'] = df[age_youth].sum(axis=1)
+    df['age_adults'] = df[age_adults].sum(axis=1)
+    df['age_senior'] = df[age_senior].sum(axis=1)
+    df.drop(age_children, axis=1, inplace=True)
+    df.drop(age_youth, axis=1, inplace=True)
+    df.drop(age_adults, axis=1, inplace=True)
+    df.drop(age_senior, axis=1, inplace=True)
+
+    """ REDUCES SCORE
+    df['age0-16'] = df['age0-16'].replace(0, df['age0-16'].mean())
+    df['age17-45'] = df['age17-45'].replace(0, df['age17-45'].mean())
+    df['age46-90'] = df['age46-90'].replace(0, df['age46-90'].mean())
+    """
 
     income_features.drop_duplicates(subset=['grunnkrets_id'])  # remove duplicates from income data
     df = pd.merge(df, income_features.drop_duplicates(subset=['grunnkrets_id']), how='left')  # merge with the income data.
-    """
-    df['normalized_income'] = (df['all_households'] - df['all_households'].min()) / (df['all_households'].max() - df['all_households'].min())
-    """
+
+    # df['all_households'] = df['all_households'].replace(0, df['all_households'].mean()) REDUCES SCORE, BUT WHY?
+
     households_features.drop_duplicates(subset=['grunnkrets_id'])  # remove duplicates from households data
     df = pd.merge(df, households_features.drop_duplicates(subset=['grunnkrets_id']), how='left')  # merge with the households data.
 
@@ -187,7 +242,7 @@ if os.path.exists(model_to_load):
 else:
     print("\nNo model found. Attempt at creating a new one will now start:")
     print("Attempting to initialize parameters for training...")
-    param = {'max_depth': 30, 'eta': 0.05, 'objective': 'reg:squarederror'}
+    param = {'max_depth': 30, 'eta': 0.1, 'objective': 'reg:squarederror'}
     num_round = 1000
     evallist = [(dtest, 'eval'), (dtrain, 'train')]
     print("--> parameters for training initialized.")
@@ -200,14 +255,10 @@ else:
     bst.save_model(model_to_load)
     print("--> model saved.")
 
-# plot_data(generate_features(train, spatial, age, income, households, plaace, busstops), 0)
-print(generate_features(train, spatial, age, income, households, plaace, busstops).info())
+# plot_data(generate_features(train, spatial, age, income, households, plaace, busstops))
 print("\nAttempting to start prediction...")
 ypred = bst.predict(dtest, ntree_limit=bst.best_iteration)
 print("--> Prediction finished.")
-
-plot_importance(bst)
-plt.show()
 
 print("\nAttempting to save prediction...")
 submission['predicted'] = np.array(ypred)
@@ -215,4 +266,5 @@ sub_features = features[1:len(features)]
 submission.to_csv('submissions/'+','.join(sub_features)+'.csv', index=False)
 print("--> prediction saved with features as name in submission folder.")
 
-
+plot_importance(bst)
+plt.show()
