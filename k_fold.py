cc138da4ef40eb10c1972888a243a65f47b6f36d
Karl Oskar Magnus Holm <koholm@stud.ntnu.no>
2022-11-02 23:33:36 +0100

    Better random k-means, age stuffs and geo stuffs

---
+++ k_fold.py
@@ -0,0 +1,89 @@
+import itertools
+import xgboost as xgb
+import numpy as np
+import time
+
+from xgboost import XGBRegressor
+from typing import Dict
+from utils import rmsle_xgb
+from sklearn.model_selection import RandomizedSearchCV, KFold, GridSearchCV
+from scipy.stats import uniform, randint
+from sklearn.metrics import make_scorer
+
+
+def _rmsle(X, y):
+    y[y < -1] = -1 + 1e-6
+    elements = np.power(np.log1p(y) - np.log1p(y), 2)
+    return 'RMSLE', float(np.sqrt(np.sum(elements) / len(y)))
+
+
+def _rmsle_vanilla(y_pred, y_true):
+    elements = np.power(np.log1p(y_pred) - np.log1p(y_true), 2)
+    return float(np.sqrt(np.sum(elements) / len(y_true)))
+
+
+rmsle_scorer = make_scorer(_rmsle_vanilla, greater_is_better=False)
+
+
+def random_k_fold(X, y, model=None, params=None, k=5, n_iter=20, verbose=10, n_jobs=-1):
+    # Parameter grid for XGBoost
+    params = {"colsample_bytree": uniform(0.3, 0.7),
+              "gamma": uniform(0, 0.5),
+              "learning_rate": uniform(0.003, 0.3),  # default 0.1
+              "max_depth": randint(2, 6),  # default 3
+              "n_estimators": randint(100, 400),  # default 100
+              "subsample": uniform(0.6, 0.4),
+              'objective': ['reg:squaredlogerror'],
+              'eval_metric': [_rmsle],
+              'min_child_weight': randint(3, 8),
+              'max_depth': randint(5, 10)} if params is None else params
+
+    kfold = KFold(n_splits=k, shuffle=True)
+
+    model = XGBRegressor() if model is None else model
+    randm_src = RandomizedSearchCV(model, param_distributions=params, n_iter=n_iter,
+                                   scoring=rmsle_scorer, verbose=verbose,
+                                   cv=kfold.split(X, y), n_jobs=n_jobs)
+
+    start = time.time()
+    model = randm_src.fit(X, y)
+    print("RandomizedSearchCV took %.2f seconds for %d candidates"
+          " parameter settings." % ((time.time() - start), n_iter))
+
+    return model
+
+
+def product_dict(**kwargs):
+    keys = kwargs.keys()
+    vals = kwargs.values()
+    lst = []
+    for instance in itertools.product(*vals):
+        lst.append(dict(zip(keys, instance)))
+    return lst
+
+
+def xgb_cross_validation(param_lists: Dict, dtrain: xgb.DMatrix, num_boost_round=100, nfold=10, metric=rmsle_xgb, early_stopping_rounds=10):
+    min_rmsle = float("Inf")
+    best_params = None
+    for params in product_dict(**param_lists):
+        # Run CV
+        cv_results = xgb.cv(
+            params,
+            dtrain,
+            num_boost_round=100,
+            seed=42,
+            nfold=5,
+            custom_metric=rmsle_xgb,
+            early_stopping_rounds=10)
+
+        # Update best RMSLE
+        mean_rmsle = cv_results['test-RMSLE-mean'].min()
+        boost_rounds = cv_results['test-RMSLE-mean'].argmin()
+        print(params)
+        print("\tRMSLE {} for {} rounds \t{}".format(mean_rmsle, boost_rounds, '(New best)' if mean_rmsle < min_rmsle else ''))
+        if mean_rmsle < min_rmsle:
+            min_rmsle = mean_rmsle
+            best_params = params
+
+    print(f"Best params: {best_params}")
+    return best_params
--- utils.py
+++ utils.py
@@ -1,10 +1,15 @@
 import xgboost as xgb
-from typing import Tuple, Dict
+from typing import List, Tuple, Dict
 import numpy as np
 import itertools
+import pandas as pd
+
+from pyproj import Geod
+from shapely.geometry import Point, LineString
+from sklearn.preprocessing import LabelEncoder
 
 
-def rmsle(predt: np.ndarray, dtrain: xgb.DMatrix) -> Tuple[str, float]:
+def rmsle_xgb(predt: np.ndarray, dtrain: xgb.DMatrix) -> Tuple[str, float]:
     ''' Root mean squared log error metric.
 
         :math:`\sqrt{\frac{1}{N}[log(pred + 1) - log(label + 1)]^2}`
@@ -39,37 +44,36 @@ def squared_log(predt: np.ndarray,
     return grad, hess
 
 
-def product_dict(**kwargs):
-    keys = kwargs.keys()
-    vals = kwargs.values()
-    lst = []
-    for instance in itertools.product(*vals):
-        lst.append(dict(zip(keys, instance)))
-    return lst
+def meter_distance(lat1, lon1, lat2, lon2):
+    line_string = LineString([Point(lon1, lat1), Point(lon2, lat2)])
+    geod = Geod(ellps="WGS84")
+    return geod.geometry_length(line_string)
 
 
-def xgb_cross_validation(param_lists: Dict, dtrain: xgb.DMatrix, num_boost_round=100, nfold=10, metric=rmsle, early_stopping_rounds=10):
-    min_rmsle = float("Inf")
-    best_params = None
-    for params in product_dict(**param_lists):
-        # Run CV
-        cv_results = xgb.cv(
-            params,
-            dtrain,
-            num_boost_round=100,
-            seed=42,
-            nfold=5,
-            custom_metric=rmsle,
-            early_stopping_rounds=10)
+def add_city_centre_dist(X: pd.DataFrame):
+    old_shape = X.shape
 
-        # Update best RMSLE
-        mean_rmsle = cv_results['test-RMSLE-mean'].min()
-        boost_rounds = cv_results['test-RMSLE-mean'].argmin()
-        print(params)
-        print("\tRMSLE {} for {} rounds \t{}".format(mean_rmsle, boost_rounds, '(New best)' if mean_rmsle < min_rmsle else ''))
-        if mean_rmsle < min_rmsle:
-            min_rmsle = mean_rmsle
-            best_params = params
+    city_centres = X.groupby(['municipality_name'])[['lat', 'lon']].apply(lambda x: x.sum() / (x.count()))[['lat', 'lon']]
+    X = X.merge(city_centres, on=['municipality_name'], how='left', suffixes=(None, '_center'))
+    assert X.shape[0] == old_shape[0]
 
-    print(f"Best params: {best_params}")
-    return best_params
+    X.fillna(value={'lat_center': X.lat, 'lon_center': X.lon}, inplace=True)
+
+    X['dist_to_center'] = X.apply(lambda row: meter_distance(row.lat, row.lon, row.lat_center, row.lon_center), axis=1)
+    assert X.shape[0] == old_shape[0]
+
+    return X
+
+
+def group_ages(age: pd.DataFrame, age_ranges: List[Tuple[int, int]]):
+    age_new = age[['grunnkrets_id', 'year']].drop_duplicates(subset=['grunnkrets_id'], keep='last')
+
+    for rng in age_ranges:
+        cols = [f'age_{age}' for age in range(rng[0], rng[1] + 1)]
+        rng_sum = age[cols].sum(axis=1).astype('int32')
+        age_new[f'age_{rng[0]}_{rng[-1]}'] = rng_sum
+
+    age = age.drop_duplicates(subset='grunnkrets_id').drop(columns=['year', *(f'age_{age}' for age in range(0, 91))], axis=1)
+    age = age.merge(age_new.drop(columns=['year']), on='grunnkrets_id')
+
+    return age
